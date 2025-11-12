123ebd93845b0a8c2d43fff13ecb8f69ee547f35
Ritikkumar81 <bcawork81@gmail.com>
2025-11-12 08:05:55 +0530

    Create Q-10. Write a program to multiply two matrices. .cpp

---
+++ Q-10. Write a program to multiply two matrices. .cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int m, n, p;
+
+    cout << "Enter the number of rows and columns of matrix A: ";
+    cin >> m >> n;
+    cout << "Enter the number of rows and columns of matrix B: ";
+    cin >> n >> p;
+
+    int A[m][n], B[n][p], C[m][p];
+
+    cout << "Enter elements of matrix A: \n";
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cin >> A[i][j];
+        }
+    }
+
+    cout << "Enter elements of matrix B: \n";
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < p; j++) {
+            cin >> B[i][j];
+        }
+    }
+
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
+            C[i][j] = 0;
+            for (int k = 0; k < n; k++) {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+
+    cout << "Resultant matrix after multiplication: \n";
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < p; j++) {
+            cout << C[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
